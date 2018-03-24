76d6c36d98d5281880ed14f346cb254715491221
Abhilash Kulkarni <abhikul1993@gmail.com>
2018-03-24 15:56:06 +0530

    server.js updated for heroku deployment

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ app.get('*', function(req, res) {
  res.sendFile(__dirname + "/src/index.html");
 })
 
-app.listen(8080);
-console.log("Listening on PORT "+ 8080);
+app.listen(process.ENV.port || 8080);
+console.log("Listening on PORT "+ process.ENV.port || 8080);
