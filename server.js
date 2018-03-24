1e2f40a13989601ac5931bf7a998d63efa7b67e6
Abhilash Kulkarni <abhikul1993@gmail.com>
2018-03-24 16:03:49 +0530

    server.js updated

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ app.get('*', function(req, res) {
  res.sendFile(__dirname + "/src/index.html");
 })
 
-app.listen(process.ENV.port || 8080);
-console.log("Listening on PORT "+ process.ENV.port || 8080);
+app.listen(process.env.PORT || 8080);
+console.log("Listening on PORT "+ process.env.PORT || 8080);
