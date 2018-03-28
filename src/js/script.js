4798b2f65cace67610609085d4e9e9147add90ce
Abhilash Kulkarni <abhikul1993@gmail.com>
2018-03-28 23:47:01 +0530

    Trying other animations

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
   $(window).bind('scroll',function(e){
     parallaxScroll();
+    if($(window).scrollTop() > 636)
+    {
+       $( ".details" ).each(function( index ) {
+         var self = this;
+         // $(self).addClass("slideUp");
+         setTimeout(function(){
+           $(self).addClass("slideUp");
+         },100*index)
+       })
+    }
   });
 
 function parallaxScroll(){
   var scrolled = $(window).scrollTop();
-  $('.background').css('top',(0-(scrolled*.75))+'px');
+  $('.background').css('top',(0-(scrolled*.25))+'px');
   $('.heading').css('top',(0-(scrolled*.25))+'px');
   $('.content').css('margin-top',( scrolled*.25)+'px');
   }
