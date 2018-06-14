937f9f9d4b04bb62abdceb801f91a18cc881336a
Abhilash Kulkarni <abhikul1993@gmail.com>
2018-06-14 23:34:13 +0530

    Better animation effect using waypoint (sample usage)
    Apply a class to an element once comes on the view point

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,29 @@
-$(document).ready(function(){
-  $(window).bind('scroll',function(e){
+$(document).ready(function() {
+  $(window).bind("scroll", function(e) {
     parallaxScroll();
-    if($(window).scrollTop() > 636)
-    {
-       $( ".details" ).each(function( index ) {
-         var self = this;
-         setTimeout(function(){
-           $(self).addClass("slideUp");
-         },100*index)
-       })
+    if ($(window).scrollTop() > 636) {
     }
   });
 
-function parallaxScroll(){
-  var scrolled = $(window).scrollTop();
-  $('.background').css('top',(0-(scrolled*.25))+'px');
-  // $('.heading').css('top',(0-(scrolled*.25))+'px');
-  $('.content').css('margin-top',( scrolled*.25)+'px');
+  function parallaxScroll() {
+    var scrolled = $(window).scrollTop();
+    $(".background").css("top", 0 - scrolled * 0.25 + "px");
+    // $('.heading').css('top',(0-(scrolled*.25))+'px');
+    $(".content").css("margin-top", scrolled * 0.25 + "px");
   }
-})
+
+  //sample waypoint usage - apply a class for an element once it on the view point
+  $(".details").each(function(index) {
+    $(this).attr("id", "item_" + index);
+    var waypoint = new Waypoint({
+      element: document.getElementById("item_" + index),
+      handler: function(direction) {
+        if (direction == "down") {
+          console.log();
+          $("#" + this.element.id).addClass("slideUp");
+        }
+      },
+      offset: "30%"
+    });
+  });
+});
